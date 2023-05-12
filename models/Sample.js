6c3a4b370fc06a75915fbb70683388e0d99d0a69
Diego Mompó Redoli <diego.mompo.redoli@gmail.com>
2023-05-12 19:38:16 +0200

    Revert "Feature/add players team relation and seed"

--- models/Sample.js
+++ models/Sample.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { SubSample } = require("./SubSaple.js");
+const { SubSample } = require("./SubSample.js");
 
 const sampleSchema = new Schema(
   {
