f3786c0d7b55974a2a8ffc0aae9bf6908ae04136
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-05-12 18:55:43 +0200

    Create Player

---
+++ models/Player.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    positionPrefer : {
+      type: String,
+      required: true,
+    },
+    number : {
+      type: Number,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Player = mongoose.model("Player", playerSchema);
+module.exports = { Player };
