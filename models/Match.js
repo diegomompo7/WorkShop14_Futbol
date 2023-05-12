4d8457b5e7d1696b653dbd956d4ce9258f38e664
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-05-12 19:32:12 +0200

    Match Information

--- models/Match.js
+++ models/Match.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { Teams } = require("./Teams.js");
 
 const matchSchema = new Schema(
   {
     localTeam: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: Teams,
+        ref: "Teams",
     },
     awayTeam: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: Teams,
+        ref: "Teams",
     },
     goalsLocalTeam : {
         type: Number,
