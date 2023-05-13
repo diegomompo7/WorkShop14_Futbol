8ea3a0f302f1a5ad3b0e913c7bd5e2e185f7d5a1
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-05-13 12:11:59 +0200

    CORS implementado

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true,
+    es2021: true
   },
-  extends: "standard",
-  overrides: [],
+  extends: 'standard',
+  overrides: [
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    "semi": "off",
-    "space-before-function-paren": "off",
-    "quotes": [2, "double"],
-    "quote-props": [2, "consistent"],
-    "multiline-ternary": "off",
-    "comma-dangle": "off",
-  },
-};
+  }
+}
